96a32c7aed0d0d3bb854607dd86fb9a3e3c2a776
michelleog2351 <L00176523@atu.ie>
2026-01-02 20:38:39 +0000

    [WIP] add initial files to the repo

---
+++ FLASK-API/api.py
@@ -0,0 +1,50 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource, Api, reqparse, fields, marshal_with, abort
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+api = Api(app)
+
+class UserModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"User(name = {self.name}, email = {self.email})"
+
+user_args = reqparse.RequestParser()
+user_args.add_argument("name", type=str, help="Name cannot be blank", required=True)
+user_args.add_argument("email", type=str, help="Email cannot be blank", required=True)
+
+userFields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'email': fields.String
+}
+
+class Users(Resource):
+    @marshal_with(userFields)
+    def get(self):
+        users = UserModel.query.all()
+        return users
+
+    @marshal_with(userFields)
+    def post(self):
+        args = user_args.parse_args()
+        user = UserModel(name=args['name'], email=args['email'])
+        db.session.add(user)
+        db.session.commit()
+        users = UserModel.query.all()
+        return users, 201
+    
+api.add_resource(Users, '/api/users')
+
+@app.route('/')
+def home():
+    return '<h1>Flask REST API</h1><p>This is a simple REST API built with Flask.</p>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
